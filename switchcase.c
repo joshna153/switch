f398baf100ea0bfde9e3b394a37b52c0dda52605
joshna153 <80655338+joshna153@users.noreply.github.com>
2021-03-16 21:16:35 +0530

    Add files via upload

--- switchcase.c
+++ switchcase.c
@@ -2,31 +2,32 @@
 #include <conio.h>
 main()
 {
-  int food item=0;
-  printf("select a food item: \n1. pizza\n2. burger\n3. pasta\n4. frenchfries\n5. sandwich");
-  scanf("%d", & fooditem);
-  switch(fooditem)
-  {
-    case 1:
-         printf("fooditem - pizza. \n");
-         printf("price - Rs 239");
-         break:
-    case 2:
-        printf("fooditem - burger. \n");
-        printf("price - Rs 129");
-        break;
-    case 3:
-        printf("fooditem - pasta. \n");
-        printf("price - Rs 179");
-        break;
-    case 4:
-        printf("fooditem - frenchfries. \n");
-        printf("price - Rs 99");
-        break;
-    case 5:
-       printf("fooditem - sandwich.\n");
-       printf("price - Rs 149");
-       break;
-    default : ("Invalid choice");
-  }
+	int fooditem=0;
+	printf("select an food item: \n1. pizza\n2. burger\n3. pasta\n4. frenchfries\n5. sandwich");
+	scanf("%d", & fooditem);
+	switch(fooditem)
+	{
+		case 1:
+			printf("fooditem - pizza. \n");
+			printf("price - Rs 239");
+			break;
+		case 2:
+			printf("fooditem - burger. \n");
+			printf("price - Rs 129");
+			break;
+		case 3:
+			printf("fooditem - pasta.\n");
+			printf("price - Rs 179");
+			break;
+		case 4:
+			printf("fooditem - frenchfries. \n");
+			printf("price - Rs 99");
+			break;
+		case 5:
+			printf("fooditem - sandwich.\n");
+			printf("price - Rs 149");
+			break;
+		default : printf("Invalid choice");
+	}
+	
 }
